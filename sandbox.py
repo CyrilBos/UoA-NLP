1ad8a2431c9583f42acf53b3fa6d3b8551cfc755
CyrilBos <bos.cyril33@gmail.com>
2017-06-10 10:00:47 +1200

    sandbox file to play around the results

---
+++ sandbox.py
@@ -0,0 +1,17 @@
+import pyLDAvis.gensim
+import gensim
+#import graphlab
+
+topics = 100
+passes = 20
+
+save_filename = "lda_questions_{}_{}".format(topics, passes)
+
+dictionary = gensim.corpora.Dictionary.load(save_filename + ".dict")
+corpus = gensim.corpora.MmCorpus(save_filename + ".mm")
+ldamodel = gensim.models.LdaMulticore.load(save_filename + ".model")
+
+vis_data = pyLDAvis.gensim.prepare(ldamodel, corpus, dictionary)
+pyLDAvis.display(vis_data)
+#pyLDAvis.enable_notebook(vis_data)
+pyLDAvis.show(vis_data)
