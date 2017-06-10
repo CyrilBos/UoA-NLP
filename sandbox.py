d03c7f1a3deab2ed7da06425fae6204594f953c9
CyrilBos <bos.cyril33@gmail.com>
2017-06-10 23:46:10 +1200

    sandbox updated with latest modifications

--- sandbox.py
+++ sandbox.py
@@ -5,11 +5,12 @@ import gensim
 topics = 100
 passes = 20
 
-save_filename = "lda_questions_{}_{}".format(topics, passes)
+save_filename = "lda-saves/lda_replies_question_forum_2287_{}_{}".format(topics, passes)
 
 dictionary = gensim.corpora.Dictionary.load(save_filename + ".dict")
 corpus = gensim.corpora.MmCorpus(save_filename + ".mm")
 ldamodel = gensim.models.LdaMulticore.load(save_filename + ".model")
+
 
 vis_data = pyLDAvis.gensim.prepare(ldamodel, corpus, dictionary)
 pyLDAvis.display(vis_data)
