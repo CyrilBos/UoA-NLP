1e7b7e889a7438985a1b2f99f31385034b495929
CyrilBos <bos.cyril33@gmail.com>
2017-06-19 11:12:21 +1200

    sandbox changes

--- sandbox.py
+++ sandbox.py
@@ -1,4 +1,3 @@
-"""
 import pyLDAvis.gensim
 import gensim
 #import graphlab
@@ -16,19 +15,3 @@ vis_data = pyLDAvis.gensim.prepare(ldamodel, corpus, dictionary)
 pyLDAvis.display(vis_data)
 #pyLDAvis.enable_notebook(vis_data)
 pyLDAvis.show(vis_data)
-"""
-import numpy as np
-import lda
-
-X = lda.datasets.load_reuters()
-vocab = lda.datasets.load_reuters_vocab()
-titles = lda.datasets.load_reuters_titles()
-print(X.shape)
-print(X.sum())
-model = lda.LDA(n_topics=20, n_iter=1500, random_state=1)
-model.fit(X)  # model.fit_transform(X) is also available
-topic_word = model.topic_word_  # model.components_ also works
-n_top_words = 8
-for i, topic_dist in enumerate(topic_word):
-    topic_words = np.array(vocab)[np.argsort(topic_dist)][:-n_top_words:-1]
-    print('Topic {}: {}'.format(i, ' '.join(topic_words)))
